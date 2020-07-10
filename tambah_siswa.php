f8c7fd9af84c945b109c892a1e51af359cbc8785
hermancahyono <cahyonoherman93@gmail.com>
2020-07-10 23:25:22 +0700

    membuat halaman tambah siswa

---
+++ tambah_siswa.php
@@ -0,0 +1,97 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CRUD Produk dengan gambar - Gilacoding</title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: salmon;
+      }
+    button {
+          background-color: salmon;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+          border: 0px;
+          margin-top: 20px;
+    }
+    label {
+      margin-top: 10px;
+      float: left;
+      text-align: left;
+      width: 100%;
+    }
+    input {
+      padding: 6px;
+      width: 100%;
+      box-sizing: border-box;
+      background: #f8f8f8;
+      border: 2px solid #ccc;
+      outline-color: salmon;
+    }
+    div {
+      width: 100%;
+      height: auto;
+    }
+    .base {
+      width: 400px;
+      height: auto;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      background: #ededed;
+    }
+    </style>
+  </head>
+  <body>
+      <center>
+        <h1>Tambah Produk</h1>
+      <center>
+      <form method="POST" action="proses_tambah.php" enctype="multipart/form-data" >
+      <section class="base">
+        <div>
+          <label>NIS</label>
+          <input type="text" name="nis" autofocus="" required="" />
+        </div>
+        <div>
+          <label>Nama Siswa</label>
+         <input type="text" name="nama" />
+        </div>
+        <div>
+          <label>Jenis Kelamin</label>
+         <input type="text" name="jns_kelamin" required="" />
+        </div>
+        <div>
+          <label>Tempat Lahir</label>
+         <input type="text" name="tmp_lahir" required="" />
+        </div>
+        <div>
+          <label>Tanggal Lahir</label>
+         <input type="date" name="tgl_lahir" required="" />
+        </div>
+        <div>
+          <label>Alamat</label>
+         <input type="text" name="alamat" required="" />
+        </div>
+        <div>
+          <label>Kelas</label>
+         <input type="text" name="kelas" required="" />
+        </div>
+        <div>
+          <label>Foto</label>
+         <input type="file" name="foto" required="" />
+        </div>
+         <button type="submit">Simpan Data Siswa</button>
+        </div>
+        </section>
+      </form>
+  </body>
+</html>
